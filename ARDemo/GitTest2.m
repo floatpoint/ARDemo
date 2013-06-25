a394e1f4f2dffe617419ec7abe419254ace4c6cb
floatpoint <chentm1020@icloud.com>
2013-06-25 13:44:57 +0800

    test2

---
+++ ARDemo/GitTest2.m
@@ -0,0 +1,13 @@
+//
+//  GitTest2.m
+//  ARDemo
+//
+//  Created by jeans on 13-6-25.
+//  Copyright (c) 2013年 floatpoint. All rights reserved.
+//
+
+#import "GitTest2.h"
+
+@implementation GitTest2
+
+@end
