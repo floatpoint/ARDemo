392a675e9072687f9ff0fcb4352a9f0c59810b9c
floatpoint <chentm1020@icloud.com>
2016-04-26 15:03:31 +0800

    公用工具类

---
+++ ARDemo/utils/CommUtils.h
@@ -0,0 +1,13 @@
+//
+//  CommUtils.h
+//  ARDemo
+//
+//  Created by jeans on 16/4/26.
+//  Copyright © 2016年 floatpoint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CommUtils : NSObject
+
+@end
---
+++ ARDemo/utils/CommUtils.m
@@ -0,0 +1,13 @@
+//
+//  CommUtils.m
+//  ARDemo
+//
+//  Created by jeans on 16/4/26.
+//  Copyright © 2016年 floatpoint. All rights reserved.
+//
+
+#import "CommUtils.h"
+
+@implementation CommUtils
+
+@end
